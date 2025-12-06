2863d2d35a598f31f385bd8837900a4a2284e8db
marwantosolve <marwantosolve@gmail.com>
2025-12-06 20:08:32 +0200

    uploading work

--- Demo1/mas_eval/mast/__init__.py
+++ Demo1/mas_eval/mast/__init__.py
@@ -1,6 +1,6 @@
 # MAST module - Failure mode taxonomy and classifier
 from mas_eval.mast.taxonomy import MASTTaxonomy, FailureCategory
-from mas_eval.mast.classifier import MASTClassifier
+from mas_eval.mast.classifier import MASTClassifier, ClassifierMode
 from mas_eval.mast.mast_fewshot import MASTFewShotLoader
 
-__all__ = ["MASTTaxonomy", "FailureCategory", "MASTClassifier", "MASTFewShotLoader"]
+__all__ = ["MASTTaxonomy", "FailureCategory", "MASTClassifier", "MASTFewShotLoader", "ClassifierMode"]
